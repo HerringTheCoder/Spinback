961fbfa00108d378551c22a91460d591c730c99e
ssandura <44547474+HerringTheCoder@users.noreply.github.com>
2019-02-28 18:46:43 +0100

    Added authorization by login and extended users table by additional fields.

--- Spinback/database/migrations/2014_10_12_000000_create_users_table.php
+++ Spinback/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('login')->unique();
+            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('department_id');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
