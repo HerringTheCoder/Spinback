79e293dff586a0c70440b241596468cd5f252368
Mariusz Zdancewicz <mariusz@zdancewicz.it>
2019-05-25 19:39:55 +0200

    Add alternative logo, fix dashboard menu

--- resources/views/commons/navigation.blade.php
+++ resources/views/commons/navigation.blade.php
@@ -1,6 +1,32 @@
-<aside class="ui inverted vertical fluid menu navigation">
-    <a class="item" href="/">
-        <b>Spinback</b>
-    </a>
+<aside class="navigation">
+    <div class="ui inverted vertical fluid menu">
+        <a class="item logo-wrapper" href="/">
+            <img src="{{ asset('images/spinback-alt.png') }}" class="logo">
+        </a>
+        <div class="item">
+            <b>Lorem</b>
+        </div>
+        <div class="item">
+            <div class="header">
+                Lorem ipsum
+            </div>
+            <div class="menu">
+                <div class="item">
+                    Lorem
+                </div>
+                <div class="item">
+                    Ipsum
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <b>Lorem</b>
+        </div>
+        <div class="item">
+            <b>Ipsum</b>
+        </div>
+    </div>
+    
+    <div style="clear: both;"></div>
 
 </aside>
