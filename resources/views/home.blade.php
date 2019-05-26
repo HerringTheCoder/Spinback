94307329e84084f33b278553be1f1879bbe87d9d
Mariusz Zdancewicz <mariusz@zdancewicz.it>
2019-05-26 02:23:07 +0200

    Style segments on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,26 +6,26 @@
         <div class="doubling four column row">
             <div class="four wide column">
                 <a href="{{ route('departments.index') }}">
-                    <div class="ui segment">
+                    <div class="ui stacked segment">
                         <h2>{{ $departments }}</h2>
                         @lang('home.departments')
                     </div>
                 </a>
             </div>
             <div class="four wide column">
-                <div class="ui segment">
+                <div class="ui stacked segment">
                     <h2>{{ $transactions }}</h2>
                     @lang('home.transactions')
                 </div>
             </div>
             <div class="four wide column">
-                <div class="ui segment">
+                <div class="ui stacked segment">
                     <h2>{{ $discs }}</h2>
                     @lang('home.discs')
                 </div>
             </div>
             <div class="four wide column">
-                <div class="ui segment">
+                <div class="ui stacked segment">
                     <h2>{{ $parcels }}</h2>
                     @lang('home.parcels')
                 </div>
